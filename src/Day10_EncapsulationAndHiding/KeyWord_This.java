b3f950ae86382a06d963ab90b130b6d8d7eaa871
柳子祺 <1662430883@qq.com>
2022-08-29 21:49:59 +0800

    Update 2022.8.29 -- Times:04

--- src/Day10_EncapsulationAndHiding/KeyWord_This.java
+++ src/Day10_EncapsulationAndHiding/KeyWord_This.java
@@ -12,10 +12,10 @@ package Day10_EncapsulationAndHiding;
  *   的使用"this.变量"的方式，表明此变量是属性，而非形参。
  *
  *  2.2 在类的构造器中，我们可以使用"this.属性"或"this.方法"的方式，调用当前正在创建的对象属性或方法。
- *  但是，通常情况下，我们都选择省略"this."。特殊情况下，如果构造器的形参和类的属性同名时，我们必须显式
+ *   但是，通常情况下，我们都选择省略"this."。特殊情况下，如果构造器的形参和类的属性同名时，我们必须显式
  *   的使用"this.变量"的方式，表明此变量是属性，而非形参。
  *
- * 3. this调用构造器
+ *  3. this调用构造器
  * 	  ① 我们在类的构造器中，可以显式的使用"this(形参列表)"方式，调用本类中指定的其他构造器
  *    ② 构造器中不能通过"this(形参列表)"方式调用自己
  *    ③ 如果一个类中有n个构造器，则最多有 n - 1构造器中使用了"this(形参列表)"
@@ -48,7 +48,6 @@ class PersonIntro{
 
 
     public PersonIntro(){
-
 //		this.eat();
         String info = "Person初始化时，需要考虑如下的1,2,3,4...(共40行代码)";
         System.out.println(info);
@@ -57,13 +56,11 @@ class PersonIntro{
     public PersonIntro(String name){
         this();
         this.name = name;
-
     }
 
     public PersonIntro(int age){
         this();
         this.age = age;
-
     }
 
     public PersonIntro(String name,int age){
@@ -73,9 +70,14 @@ class PersonIntro{
         //Person初始化时，需要考虑如下的1,2,3,4...(共40行代码)
     }
 
+
+    //**************************************************
+    //构造JavaBean
     public void setName(String name){
         this.name = name;
     }
+    //假若使用public void etName(String name){name = name;}
+    //这样的时候两个name均为这个方法内的name，程序逻辑出错
     public String getName(){
         return this.name;
     }
@@ -85,11 +87,14 @@ class PersonIntro{
     public int getAge(){
         return this.age;
     }
+    //**************************************************
+
 
     public void eat(){
         System.out.println("人吃饭");
         this.study();
     }
+
     public void study(){
         System.out.println("人学习");
     }
